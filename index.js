c78afbdd8723d5e886ebeb694c2b6c2606968c25
Jennifer Wolan <jenniferwolan@gmail.com>
2022-12-25 14:13:27 -0600

    commit tree 1741b2a65bc00f80b1fd94b8f46c1e9241a1b78c

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 function addingEventListener() {
+    const input = document.getElementById('button');
+
+function clickAlert() {
+  alert('I was clicked!');
 }
+
+input.addEventListener('click', clickAlert);
+}
+
+//const input = document.getElementById('button');
+//input.addEventListener('click', function() {
+  //alert('I was clicked!');
+//});
+
+//const input = document.getElementById('button');
+
+//function clickAlert() {
+//  alert('I was clicked!');
+//}
+
+//input.addEventListener('click', clickAlert);
